111520dfb5ac8bad839335597d947da0bfabca84
Jonnathan M. Cazarotto <jmcazarotto@gmail.com>
2017-10-23 22:39:45 -0200

    Aula 1 de verdade

---
+++ MeuPrimeiroBeckendN/Web/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //ajustando serializacao para WebAPI
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.
+                SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters
+                .XmlFormatter);
+        }
+    }
+}
